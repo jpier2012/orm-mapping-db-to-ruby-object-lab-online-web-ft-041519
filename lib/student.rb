fef470d5644d72826298008a7f7b4b96a092ecac
James Pier <jpier2012@gmail.com>
2019-05-17 16:46:42 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -32,7 +32,7 @@ class Student
       WHERE students.name = \'#{name}\'
     SQL
 
-    DB[:conn].execute(sql).map do |row|
+    DB[:conn].execute(sql, name).map do |row|
       self.new_from_db(row)
     end
   end
