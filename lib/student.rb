b70bbdd18ffeb8b6d5d2a3a44b5b1a04211a0456
James Pier <jpier2012@gmail.com>
2019-05-17 16:41:38 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -3,7 +3,7 @@ class Student
 
   def self.new_from_db(row)
     # create a new Student object given a row from the database
-    new_student = Student.new.tap do |s|
+    new_student = self.new.tap do |s|
       s.id = row[0]
       s.name = row[1]
       s.grade = row[2]
